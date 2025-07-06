2fd50e327dd055f907b172898236e3713c51fe55
Jesse Maarleveld <jessemaarleveld@gmail.com>
2025-07-06 14:49:17 +0200

    more walker sutff

--- src/walker.rs
+++ src/walker.rs
@@ -17,12 +17,118 @@ pub(crate) trait GraphExplorer {
     fn neighbours(&self, node: NodeRef) -> Result<HashMap<NodeRef, usize>, HetNetError>;
 }
 
-pub trait GraphWalker {
-    
+pub trait NeighbourSelector {
+    fn select(&mut self, from: &[(NodeRef, usize)]) -> NodeRef;
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////
-// Types
+// Neighbour Selectors
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
+pub struct UnweightedNeighbourSelector<T: rand::Rng> {
+    rng: T
+}
+
+impl Default for UnweightedNeighbourSelector<rand::rngs::ThreadRng> {
+    fn default() -> Self {
+        Self::with_rng(rand::rng())
+    }
+}
+
+impl<T: rand::Rng> UnweightedNeighbourSelector<T> {
+    fn with_rng(rng: T) -> Self {
+        UnweightedNeighbourSelector { rng }
+    }
+}
+
+impl<T: rand::Rng> NeighbourSelector for UnweightedNeighbourSelector<T> {
+    fn select(&mut self, from: &[(NodeRef, usize)]) -> NodeRef {
+        let index = self.rng.random_range(0..from.len());
+        from[index].0
+    }
+}
+
+
+pub struct WeightedNeighbourSelector<T: rand::Rng> {
+    rng: T
+}
+
+impl Default for WeightedNeighbourSelector<rand::rngs::ThreadRng> {
+    fn default() -> Self {
+        Self::with_rng(rand::rng())
+    }
+}
+
+impl<T: rand::Rng> WeightedNeighbourSelector<T> {
+    fn with_rng(rng: T) -> Self {
+        WeightedNeighbourSelector { rng }
+    }
+}
+
+impl<T: rand::Rng> NeighbourSelector for WeightedNeighbourSelector<T> {
+    fn select(&mut self, from: &[(NodeRef, usize)]) -> NodeRef {
+        let mut hist = Vec::new();
+        let mut total = 0;
+        for (_, count) in from.iter().copied() {
+            total += count;
+            hist.push(total);
+        }
+
+        let selected = self.rng.random_range(0..total);
+        let index = hist.partition_point(|&x| x <= selected);
+        from[index].0
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
+// Random Walker
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+pub struct RandomWalkConfig {
+    path_length: usize
+}
+
+impl Default for RandomWalkConfig {
+    fn default() -> Self {
+        Self {
+            path_length: 10
+        }
+    }
+}
+
+impl RandomWalkConfig {
+    pub fn with_path_length(mut self, path_length: usize) -> Self {
+        self.path_length = path_length;
+        self
+    }
+}
+
+
+pub struct RandomWalker<G: GraphExplorer, N: NeighbourSelector> {
+    explorer: G,
+    selector: N,
+    config: RandomWalkConfig,
+}
+
+impl<G: GraphExplorer, N: NeighbourSelector> RandomWalker<G, N> {
+    pub fn new(explorer: G, selector: N, config: RandomWalkConfig) -> Self {
+        RandomWalker { explorer, selector, config }
+    }
+
+    pub fn walk_from(&mut self, start: NodeRef) -> Result<Vec<NodeRef>, HetNetError> {
+        let mut path = vec![start];
+        let mut current = start;
+
+        for _ in 0..self.config.path_length {
+            let neighbours = self.explorer.neighbours(current)?;
+            let histogram = neighbours.into_iter().collect::<Vec<_>>();
+            current = self.selector.select(&histogram);
+            path.push(current);
+        }
+
+        Ok(path)
+    }
+}
