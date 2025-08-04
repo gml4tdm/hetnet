cb15ec4095daee0ef65e4d114bc2786079ffa54d
Jesse Maarleveld <jessemaarleveld@gmail.com>
2025-08-04 23:15:23 +0200

    Distance

--- rust/hetnet/src/algorithms/distance/dijkstra.rs
+++ rust/hetnet/src/algorithms/distance/dijkstra.rs
@@ -1,4 +1,8 @@
-use std::collections::BinaryHeap;
+use std::collections::{HashSet, VecDeque};
+use std::sync::Arc;
+
+use crate::builder::next_graph_id;
+use crate::graph::GraphMetadata;
 use crate::HeteroDiGraph;
 
 impl HeteroDiGraph {
@@ -10,20 +14,61 @@ impl HeteroDiGraph {
         } else {
             self.bfs_distance_matrix()
         };
+        Self {
+            node_metadata: self.node_metadata.clone(),
+            edge_metadata: self.edge_metadata.clone(),
+            graph_metadata: Arc::new(
+                GraphMetadata {
+                    weighted_distance_matrix: self.graph_metadata.weighted_distance_matrix.clone(),
+                    next_edge_id: self.graph_metadata.next_edge_id,
+                    is_markov: self.graph_metadata.is_markov,
+                    distance_matrix: Some(
+                        Arc::new(
+                            matrix.into_iter().map(|x| x as usize).collect()
+                        )
+                    ),
+                }
+            ),
+            uid: next_graph_id(),
+            nodes: self.nodes.clone()
+        }
+    }
+
+    fn init_distance_matrix(&self) -> Vec<i64> {
+        let v = self.nodes.len();
+        let mut matrix = vec![-1i64; v*v];
+        for i in 0..v {
+            matrix[i + v*i] = 0;
+        }
+        matrix
     }
 
     fn bfs_distance_matrix(&self) -> Vec<i64> {
+        let v = self.nodes.len();
+        let mut matrix = self.init_distance_matrix();
 
+        for i in 0..v {
+            let mut seen = HashSet::new();
+            let mut queue = VecDeque::new();
+            queue.push_back((&self.nodes[i], 0));
+            while let Some((current, dist)) = queue.pop_front() {
+                if seen.contains(&current.uid) {
+                    continue;
+                }
+                matrix[i + v*current.uid] = dist;
+                seen.insert(current.uid);
+                for edge in current.connections.iter() {
+                    queue.push_back((&self.nodes[edge.to.0], dist + 1));
+                }
+            }
+        }
+        
+        matrix
     }
 
     fn floyd_warshall_distance_matrix(&self) -> Vec<i64> {
         let v = self.nodes.len();
-        let mut matrix = vec![-1i64; v*v];
-
-        // Self-connections are 0
-        for i in 0..v {
-            matrix[i + v*i] = 0;
-        }
+        let mut matrix = self.init_distance_matrix();
 
         // Initialise matrix using matrix connections
         for (i, node) in self.nodes.iter().enumerate() {
