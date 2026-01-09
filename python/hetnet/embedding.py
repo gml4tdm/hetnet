8050329ea88588743afe745f3ceaf1b2ac5e1b0c
Jesse Maarleveld <jessemaarleveld@gmail.com>
2026-01-09 10:37:39 +0100

    Fix parallelism in Node2Vec; add Node2Vec reporting hooks

--- python/hetnet/embedding.py
+++ python/hetnet/embedding.py
@@ -7,6 +7,17 @@ from .core import Graph, NodeRef
 from ._node2vec import AbstractNode2Vec, Node2Vec as DefaultNode2Vec
 
 
+class _ReporterWrapper:
+
+    def __init__(self, reporter: typing.Callable[[int, str], None]):
+        self.reporter = reporter
+        self.last_message = None
+
+    def __call__(self, progress: int, message: str):
+        if self.last_message != message:
+            self.reporter(progress, message)
+            self.last_message = message
+
 
 def node2vec(g: Graph, *,
              node2vec_model: type[AbstractNode2Vec] = DefaultNode2Vec,
@@ -26,10 +37,13 @@ def node2vec(g: Graph, *,
              batch_size: int = 32,
              sparse: bool = False,
              epochs: int = 5,
+             progress_reporter: typing.Callable[[int, str], None] = lambda x, y: None,
              num_threads: int = 1,
              num_workers: int = 1,
              device_hint: str | None = None,
              fast_walker: bool = False) -> tuple[torch.Tensor, dict[NodeRef, int], list[float]]:
+    progress_reporter = _ReporterWrapper(progress_reporter)
+
     if device_hint is None:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     else:
@@ -47,6 +61,7 @@ def node2vec(g: Graph, *,
     #     num_negative_samples=num_negative_samples,
     #     sparse=sparse,
     # )
+    progress_reporter(0, 'Initialising model')
     model = node2vec_model(
         g,
         weighted=weighted,
@@ -65,6 +80,8 @@ def node2vec(g: Graph, *,
     )
     model = model.to(device)
 
+    progress_reporter(0, 'Model ready')
+
     loader = model.loader(batch_size=batch_size, shuffle=True, num_workers=num_workers)
 
     if sparse:
@@ -72,6 +89,10 @@ def node2vec(g: Graph, *,
     else:
         optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+    progress_reporter(0, 'Training')
+    n_steps_per_epoch = len(loader)
+    n_total_steps = epochs * n_steps_per_epoch
+    n_steps = 0
     norm = len(loader) * (1 + model.num_negative_samples)
     losses = []
     for epoch in range(epochs):
@@ -82,8 +103,15 @@ def node2vec(g: Graph, *,
             loss.backward()
             optimiser.step()
             total_loss += loss.item()
+            n_steps += 1
+            progress_reporter(
+                n_steps * 100 // n_total_steps,
+                f'Epoch {epoch} {(n_steps % n_steps_per_epoch) * 100 // n_steps_per_epoch:.2f}% done'
+            )
         losses.append(total_loss / norm)
-        print(f'Epoch {epoch}: {total_loss / norm:.4f}')
+        #print(f'Epoch {epoch}: {total_loss / norm:.4f}')
+
+    progress_reporter(100, 'Done')
 
     embeddings = model.embedding.weight.detach().cpu()
     return embeddings, model.node_to_index_mapping, losses
