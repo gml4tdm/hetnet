d13d2b0d0154c10aee6f77b345236e5498691e64
Jesse Maarleveld <j.maarleveld@rug.nl>
2026-03-04 13:17:19 +0100

    Add PyTorch thread control for Node2Vec

--- python/hetnet/embedding.py
+++ python/hetnet/embedding.py
@@ -121,6 +121,11 @@ def node2vec(g: Graph, *,
              fast_walker: bool = False,
              **kwargs) -> tuple[torch.Tensor, dict[NodeRef, int], list[float]]:
     progress_reporter = _ReporterWrapper(progress_reporter)
+
+    if torch.get_num_threads() != num_threads:
+        warnings.warn(f'Setting torch num_threads to {num_threads}')
+        torch.set_num_threads(num_threads)
+
     device = _get_device(device_hint)
 
     progress_reporter(0, 'Initialising model')
