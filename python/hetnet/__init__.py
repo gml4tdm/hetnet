5b5d929119c9e28acf171956d16c9af5c4c74ad2
Jesse Maarleveld <j.maarleveld@rug.nl>
2026-04-12 01:22:17 +0200

    Implement igraph support

--- python/hetnet/__init__.py
+++ python/hetnet/__init__.py
@@ -7,5 +7,6 @@ from .neo4j_support import write_pseudo_json as write_pseudo_json_neo4j
 from .neo4j_support import load_graph_streaming as load_graph_streaming_neo4j
 from .neo4j_support import ProgressReporter, default_reporter
 from . import networkx_support as networkx
+from . import igraph_support as igraph
 from .scripting import parse_script
 #from . import embedding
---
+++ python/hetnet/igraph_support.py
@@ -0,0 +1,46 @@
+import igraph
+
+from . import core
+
+
+class _IdMapper(dict):
+    def __missing__(self, key):
+        n = len(self)
+        self[key] = n
+        return len(self)
+
+    def to_strict_dict(self):
+        return {k: v for k, v in self.items()}
+
+
+def to_igraph_graph(g: core.Graph) -> igraph.Graph:
+    ig, _ = _to_igraph_graph(g)
+    return ig
+
+
+def _to_igraph_graph(g: core.Graph):
+    mapper = _IdMapper()
+    edges = g.edge_list()
+    edge_list = [
+        (mapper[edge.source], mapper[edge.destination])
+        for edge in edges
+    ]
+    weights = [edge.weight for edge in edges]
+    ig = igraph.Graph(edge_list)
+    ig.es['weight'] = weights
+    return ig, mapper.to_strict_dict()
+
+
+def personalised_pagerank(g: core.Graph,
+                          alpha: float,
+                          nodes: list[core.NodeRef]) -> dict[core.NodeRef, float]:
+    ig, mapping = _to_igraph_graph(g)
+    scores = ig.personalized_pagerank(
+        vertices=None,
+        return_vertices=[mapping[node] for node in nodes],
+        weights='weight',
+        damping=alpha,
+        directed=True,
+    )
+    rev_mapping = {v: k for k, v in mapping.items()}
+    return {rev_mapping[i]: score for i, score in enumerate(scores)}
