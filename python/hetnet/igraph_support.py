10aeaf991d63fb5a95038d5e7e76e023d82e70c8
Jesse Maarleveld <j.maarleveld@rug.nl>
2026-04-12 02:25:18 +0200

    more stuff

--- python/hetnet/igraph_support.py
+++ python/hetnet/igraph_support.py
@@ -13,12 +13,22 @@ class _IdMapper(dict):
         return {k: v for k, v in self.items()}
 
 
-def to_igraph_graph(g: core.Graph) -> igraph.Graph:
-    ig, _ = _to_igraph_graph(g)
+def to_igraph_components(g: core.Graph):
+    return _to_igraph_components(g)[0]
+
+
+def to_igraph_components_and_mapping(g: core.Graph):
+    return _to_igraph_components(g)
+
+
+def from_igraph_components(components):
+    edge_list, weights = components
+    ig = igraph.Graph(edge_list)
+    ig.es['weight'] = weights
     return ig
 
 
-def _to_igraph_graph(g: core.Graph):
+def _to_igraph_components(g: core.Graph):
     mapper = _IdMapper()
     edges = g.edge_list()
     edge_list = [
@@ -26,21 +36,39 @@ def _to_igraph_graph(g: core.Graph):
         for edge in edges
     ]
     weights = [edge.weight for edge in edges]
-    ig = igraph.Graph(edge_list)
-    ig.es['weight'] = weights
-    return ig, mapper.to_strict_dict()
+    return (edge_list, weights), mapper.to_strict_dict()
 
 
-def personalised_pagerank(g: core.Graph,
+def to_igraph_graph(g: core.Graph) -> igraph.Graph:
+    ig, _ = _to_igraph_graph(g)
+    return ig
+
+
+def _to_igraph_graph(g: core.Graph):
+    components, mapper = _to_igraph_components(g)
+    return from_igraph_components(components), mapper
+
+
+def personalised_pagerank(g: igraph.Graph | core.Graph,
                           alpha: float,
-                          nodes: list[core.NodeRef]) -> dict[core.NodeRef, float]:
-    ig, mapping = _to_igraph_graph(g)
-    scores = ig.personalized_pagerank(
-        vertices=None,
-        reset_vertices=[mapping[node] for node in nodes],
-        weights='weight',
-        damping=alpha,
-        directed=True,
-    )
-    rev_mapping = {v: k for k, v in mapping.items()}
-    return {rev_mapping[i]: score for i, score in enumerate(scores)}
+                          nodes: list[core.NodeRef] | list[int]) -> dict[int, float] | dict[core.NodeRef, float]:
+    if isinstance(g, core.Graph):
+        ig, mapping = _to_igraph_graph(g)
+        scores = ig.personalized_pagerank(
+            vertices=None,
+            reset_vertices=[mapping[node] for node in nodes],
+            weights='weight',
+            damping=alpha,
+            directed=True,
+        )
+        rev_mapping = {v: k for k, v in mapping.items()}
+        return {rev_mapping[i]: score for i, score in enumerate(scores)}
+    else:
+        scores = g.personalized_pagerank(
+            vertices=None,
+            reset_vertices=nodes,
+            weights='weight',
+            damping=alpha,
+            directed=True,
+        )
+        return {node: score for node, score in enumerate(scores)}
